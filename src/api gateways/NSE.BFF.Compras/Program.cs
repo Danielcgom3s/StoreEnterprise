6c46f795057601f24fa752ebc946847105414cb2
Daniel <daniel.gomes@woli.tec.br>
2022-08-03 12:22:56 -0300

    First Commit

---
+++ src/api gateways/NSE.BFF.Compras/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using NSE.Bff.Compras;
+
+namespace NSE.BFF.Compras
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
